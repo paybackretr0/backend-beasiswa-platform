2fb687d41d6b5fecc5e6c1a2c52a70fd57cbcfa8
Khalied Nauly Maturino <khalidmaturino@gmail.com>
2025-10-01 22:47:10 +0700

    Website Feature Done

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const authRoutes = require("./auth.routes");
 const userRoutes = require("./user.routes");
 const facultyRoutes = require("./faculty.routes");
 const departmentRoutes = require("./department.routes");
+const websiteRoutes = require("./website.routes");
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/faculties", facultyRoutes);
 router.use("/departments", departmentRoutes);
+router.use("/websites", websiteRoutes);
 
 module.exports = router;
