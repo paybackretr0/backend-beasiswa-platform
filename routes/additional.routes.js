8119f8d759b6f405fdb1a7c0355729f7c0d98908
Khalied Nauly Maturino <khalidmaturino@gmail.com>
2026-01-15 12:45:08 +0700

    Route CRUD Template Comment superadmin

--- routes/additional.routes.js
+++ routes/additional.routes.js
@@ -5,6 +5,13 @@ const {
   createBackup,
   getAllActivityLogs,
   exportActivityLogsToExcel,
+  getAllCommentTemplates,
+  getCommentTemplateById,
+  createCommentTemplate,
+  updateCommentTemplate,
+  getActiveTemplatesByType,
+  activateCommentTemplate,
+  deactivateCommentTemplate,
 } = require("../controllers/additional.controller");
 const {
   authenticate,
@@ -12,10 +19,29 @@ const {
 } = require("../middlewares/auth.middleware");
 const authorize = require("../middlewares/role.middleware");
 
+router.get(
+  "/comment-templates/active/:type",
+  authenticate,
+  verifiedUser,
+  authorize([
+    "VERIFIKATOR_FAKULTAS",
+    "VERIFIKATOR_DITMAWA",
+    "VALIDATOR_DITMAWA",
+  ]),
+  getActiveTemplatesByType
+);
+
 router.use(authenticate, verifiedUser, authorize("SUPERADMIN"));
 router.get("/backups", getAllBackups);
 router.post("/backups", createBackup);
 router.get("/activity-logs", getAllActivityLogs);
 router.get("/activity-logs/export", exportActivityLogsToExcel);
+
+router.get("/comment-templates", getAllCommentTemplates);
+router.get("/comment-templates/:id", getCommentTemplateById);
+router.post("/comment-templates", createCommentTemplate);
+router.put("/comment-templates/:id", updateCommentTemplate);
+router.patch("/comment-templates/activate/:id", activateCommentTemplate);
+router.patch("/comment-templates/deactivate/:id", deactivateCommentTemplate);
 
 module.exports = router;
